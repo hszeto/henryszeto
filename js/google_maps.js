76dbbd55c8cf151c4675ecacef6a3138353f4ebf
Henry Szeto <henryszeto@gmail.com>
2018-09-09 03:10:00 -0700

    major redesign

---
+++ js/google_maps.js
@@ -0,0 +1,63 @@
+var map;  // Google map object
+
+// Initialize and display a google map
+function initMap()
+{
+  // HTML5/W3C Geolocation
+  if ( navigator.geolocation ){
+    navigator.geolocation.getCurrentPosition( UserLocation );
+  // Default to Temple City, CA
+  }else{
+    ShowLocation( 34.1028, -118.0581, "Temple City, CA" );
+  };
+}
+
+// Callback function for asynchronous call to HTML5 geolocation
+function UserLocation( position )
+{
+  ShowLocation( position.coords.latitude, position.coords.longitude, "This is your Location" );
+}
+
+// Display a map centered at the specified coordinates with markers and InfoWindow.
+function ShowLocation( lat, lng, title )
+{
+  // Create Google coordinates for where to center the map
+  var latlng = new google.maps.LatLng( lat, lng );
+  var templeCity = new google.maps.LatLng(34.1028,-118.0581); 
+
+  // Map options for how to display the Google map
+  var mapOptions = { zoom: 12 };
+  
+  // Show the Google map in the div with the attribute id 'map-canvas'.
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // Place a Google Marker at Temple City
+  var marker = new google.maps.Marker( { 
+    position: templeCity,     
+    map: map,
+    title: "I am here at Temple City",
+    icon: 'img/myMarker.png'
+  });
+  
+  // Place a Google Marker at the visitor location 
+  // When you hover over the marker, it will display the title
+  var marker = new google.maps.Marker( { 
+    position: latlng,     
+    map: map,      
+    title: title
+  });
+
+  // Create an InfoWindow for the marker
+  var contentString = "<b>" + title + "</b>"; // HTML text to display in the InfoWindow
+  var infowindow = new google.maps.InfoWindow( { content: contentString } );
+  
+  // Set event to display the InfoWindow anchored to the marker when the marker is clicked.
+  google.maps.event.addListener( marker, 'click', function() { infowindow.open( map, marker ); });
+
+  // Center the map between the 2 markers. Temple City and visitor location.
+  var bounds = new google.maps.LatLngBounds();
+    bounds.extend(latlng);
+    bounds.extend(templeCity);
+    map.fitBounds(bounds);
+
+}
---
+++ js/main.js
@@ -0,0 +1,72 @@
+function navBar() {
+  var x = document.getElementById("mainNavbar");
+  if (x.className === "navbar") {
+      x.className += " responsive";
+  } else {
+      x.className = "navbar";
+  }
+};
+
+function stickyNav() {
+  var navbar = document.getElementById("mainNavbar");
+  var sticky = navbar.offsetTop;
+
+  window.onscroll = function() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  };
+};
+
+function projectDesc() {
+  $('.project-container')
+    .on('mouseenter','.project',function(){
+      $(this).find('.projectDesc').fadeIn();
+    })
+    .on('mouseleave','.project',function(){
+      $(this).find('.projectDesc').fadeOut();
+    });
+}
+
+function initScrollAnimations() {
+  $('#content-wrapper').css('display','block');
+  var controller = $.superscrollorama();
+
+  // title tweens
+  $('.title-line span').each(function() {
+    controller.addTween(10, TweenMax.to(this, .5, {css:{top: Math.random()*-200-600, left: (Math.random()*1000)-500, rotation:Math.random()*720-360, 'font-size': Math.random()*300+150}, ease:Quad.easeOut}),200);
+  });
+  controller.addTween(10, TweenMax.to($('#title-line1'), .75, {css:{top: 600}, ease:Quad.easeOut}),200);
+  controller.addTween(10, TweenMax.to($('#title-line2'), .75, {css:{top: 200}, ease:Quad.easeOut}),200);
+  controller.addTween(10, TweenMax.to($('#title-line3'), .75, {css:{top: -100}, ease:Quad.easeOut},200));
+}
+
+$(document).ready(function() {
+  $('body').css('visibility','visible');
+
+  $('#title-line1').lettering();
+  $('#title-line2').lettering();
+  $('#title-line3').lettering();
+
+  (new TimelineLite({onComplete:initScrollAnimations}))
+    .from( $('#title-line1 span'), .4, {delay: 1, css:{right:'1000px'}, ease:Back.easeOut})
+    .from( $('#title-line2'), .4, {css:{top:'1000px',opacity:'0'}, ease:Expo.easeOut})
+    .append([
+      TweenMax.from( $('#title-line3 .char1'), .25+Math.random(), {css:{top: '-200px', right:'1000px'}, ease:Elastic.easeOut}),
+      TweenMax.from( $('#title-line3 .char2'), .25+Math.random(), {css:{top: '300px', right:'1000px'}, ease:Elastic.easeOut}),
+      TweenMax.from( $('#title-line3 .char3'), .25+Math.random(), {css:{top: '-400px', right:'1000px'}, ease:Elastic.easeOut}),
+      TweenMax.from( $('#title-line3 .char4'), .25+Math.random(), {css:{top: '-200px', left:'1000px'}, ease:Elastic.easeOut}),
+      TweenMax.from( $('#title-line3 .char5'), .25+Math.random(), {css:{top: '200px', left:'1000px'}, ease:Elastic.easeOut})
+    ])
+    .from("#newversion", .4, {scale: 5, autoAlpha: 0, ease: Elastic.easeOut})
+    .to( $('#title-info'), .5, {css:{opacity:.99, 'margin-top':0}, delay:-1, ease:Quad.easeOut})
+    .to( $('#download'), .25, {autoAlpha:1});
+
+  // init Smooth Scroller
+  $SS().start(2000);
+
+  stickyNav();
+  projectDesc();
+});
